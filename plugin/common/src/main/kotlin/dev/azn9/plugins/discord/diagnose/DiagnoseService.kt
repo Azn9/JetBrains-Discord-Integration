59becef638250df7717894102aa39aaf97871310
Axel Joly <azn9.pro@gmail.com>
2024-12-08 12:00:42 +0100

    fix: fix incorrect browser detection (#218)

--- plugin/common/src/main/kotlin/dev/azn9/plugins/discord/diagnose/DiagnoseService.kt
+++ plugin/common/src/main/kotlin/dev/azn9/plugins/discord/diagnose/DiagnoseService.kt
@@ -149,7 +149,7 @@ class DiagnoseService : DisposableCoroutineScope {
         val discordClientNotRunning = allLines.none { line -> discord.any { exe -> line.startsWith(exe, true) } }
         if (discordClientNotRunning) {
             val discordBrowser = allLines.any { line ->
-                browsers.any { browser -> line.startsWith(browser, true) }
+                browsers.any { browser -> line.startsWith(browser, true)  && line.lowercase().contains("discord") }
             }
             if (discordBrowser) {
                 return Discord.BROWSER
